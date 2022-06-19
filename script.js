bb90c2d1ea11a0f46b3281e87e317f06125b80da
Mehdi SOMRANI <62431464+Somranii@users.noreply.github.com>
2022-06-19 18:47:59 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -15,6 +15,8 @@ $(document).ready(function(){
         }
     });
 
+
+
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
@@ -84,3 +86,27 @@ function sendEmail()
     window.location = "mailto:mehdisomrani04@gmail.com";
 }
 
+$(document).ready(function(){
+    // Add smooth scrolling to all links
+    $("a").on('click', function(event) {
+  
+      // Make sure this.hash has a value before overriding default behavior
+      if (this.hash !== "") {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+  
+        // Store hash
+        var hash = this.hash;
+  
+        // Using jQuery's animate() method to add smooth page scroll
+        // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+        $('html, body').animate({
+          scrollTop: $(hash).offset().top
+        }, 800, function(){
+     
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        });
+      } // End if
+    });
+  });
